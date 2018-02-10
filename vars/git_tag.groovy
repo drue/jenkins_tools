9689bdd2b017e0aa5cdbda3e99211f1bbe5a8407
Andrew Loewenstern <drue@gigagig.org>
2018-02-09 21:01:13 -0800

    simplify api and have name() return empty string instead of null

--- vars/git_tag.groovy
+++ vars/git_tag.groovy
@@ -2,20 +2,20 @@
 /** https://wiki.jenkins.io/display/JENKINS/Git+Tag+Message+Plugin **/
 
 /** @return The tag name, or `null` if the current commit isn't a tag. */
-String gitTagName() {
+String name() {
     commit = getCommit()
     if (commit) {
         desc = sh(script: "git describe --tags ${commit}", returnStdout: true)?.trim()
-        if (isTag(desc)) {
+        if (is_tag(desc)) {
             return desc
         }
     }
-    return null
+    return ""
 }
 
 /** @return The tag message, or `null` if the current commit isn't a tag. */
-String gitTagMessage() {
-    name = gitTagName()
+String message() {
+    name = name()
     msg = sh(script: "git tag -n10000 -l ${name}", returnStdout: true)?.trim()
     if (msg) {
         return msg.substring(name.size()+1, msg.size())
@@ -23,12 +23,12 @@ String gitTagMessage() {
     return null
 }
 
-String getCommit() {
+String commit() {
     return sh(script: 'git rev-parse HEAD', returnStdout: true)?.trim()
 }
 
 @NonCPS
-boolean isTag(String desc) {
+boolean is_tag(String desc) {
     match = desc =~ /.+-[0-9]+-g[0-9A-Fa-f]{6,}$/
     result = !match
     match = null // prevent serialisation
